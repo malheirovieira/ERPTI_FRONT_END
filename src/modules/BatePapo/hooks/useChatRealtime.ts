753f13ce7d4f9bbc2f65c42f2bca66a60fe836cf
malheirovieira <malheirovieira.g@gmail.com>
2026-07-02 11:00:48 -0300

    Feat: Criacao de arquivo e teste de código

---
+++ src/modules/BatePapo/hooks/useChatRealtime.ts
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+
+export function useChatRealtime() {
+  useEffect(() => {
+    // Inicialmente vazio.
+    // Será responsável pela conexão global do chat.
+  }, []);
+}
