88af1fe0ca410d1e130a577cf5cf5fe18b0f7d0e
malheirovieira <malheirovieira.g@gmail.com>
2026-07-02 13:59:05 -0300

    Test: exclus]ao de pasta desnecessaria

--- src/modules/BatePapo/hooks/useChatRealtime.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export function useChatRealtime() {
-  useEffect(() => {
-    // Inicialmente vazio.
-    // Será responsável pela conexão global do chat.
-  }, []);
-}
